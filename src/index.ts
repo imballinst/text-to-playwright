de80d9bf600e6a643918c97cc4db729a6e269a1a
Try Ajitiono <ballinst@gmail.com>
2025-02-20 16:12:23 +0000

    modifier finished, now we just need to use the queries to query by id

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { readFile } from 'fs/promises';
 import path from 'path';
 import { chromium } from 'playwright';
-import { parseInputTestFile } from './parser/input';
+import { parseArgs } from 'util';
+import { InputStructure, parseInputTestFile, Selector } from './parser/input';
 import { runTests } from './runner';
+
+const args = parseArgs({ options: { selector: { type: 'string', default: 'label' } } });
 
 async function runPlaywrightExample() {
   const fileContent = await readFile(path.join(process.cwd(), 'assets/test/index.html'), 'utf-8');
@@ -14,7 +17,51 @@ async function runPlaywrightExample() {
   await page.setContent(fileContent);
 
   const parsedTestFile = parseInputTestFile(testFileContent);
-  await runTests(page, parsedTestFile);
+  if (args.values.selector === 'data-qa-id') {
+    const array: Array<{ name: string; modifier: (parsedTestFile: InputStructure) => InputStructure }> = [
+      {
+        name: 'root',
+        modifier: (parsed) => {
+          const cloned = structuredClone(parsed) as InputStructure;
+          cloned.selector = Selector.parse(args.values.selector);
+          return cloned;
+        }
+      },
+      {
+        name: 'test',
+        modifier: (parsed) => {
+          const cloned = structuredClone(parsed) as InputStructure;
+          for (const test of cloned.tests) {
+            test.selector = Selector.parse(args.values.selector);
+          }
+          return cloned;
+        }
+      }
+    ];
+
+    for (const item of array) {
+      console.info(`----------------------------------------------------------------`);
+      console.info(`➡ Testing with selector data-qa-id on the ${item.name} level...`);
+      console.info(`----------------------------------------------------------------`);
+
+      const effectiveTestFile = item.modifier(parsedTestFile);
+
+      for (const test of effectiveTestFile.tests) {
+        for (let i = 0; i < test.steps.length; i++) {
+          test.steps[i] = test.steps[i].replace(/"([\w\s]+)" (link|button|input|element)/g, (...matches) => {
+            if (!matches) return '';
+
+            const [, object, elementType] = matches;
+            return `"${object.toLowerCase().replace(/\s+/g, '-')}-real-section" ${elementType}`;
+          });
+        }
+      }
+
+      // await runTests(page, effectiveTestFile);
+    }
+  } else {
+    await runTests(page, parsedTestFile);
+  }
 
   await browser.close();
   console.info(`✅ All tests completed!`);
--- src/parser/input.ts
+++ src/parser/input.ts
@@ -1,17 +1,21 @@
 import yaml from 'yaml';
 import { z } from 'zod';
 
+export const Selector = z.union([z.literal('label'), z.literal('data-qa-id')]);
+
 const TestCase = z.object({
   name: z.string(),
+  selector: Selector.optional(),
   steps: z.array(z.string())
 });
 
 const InputStructure = z.object({
+  selector: Selector.optional(),
   tests: z.array(TestCase)
 });
 export interface InputStructure extends z.infer<typeof InputStructure> {}
 
-export function parseInputTestFile(fileContent: string) {
+export function parseInputTestFile(fileContent: string): InputStructure {
   const parsed = yaml.parse(fileContent);
 
   return InputStructure.parse(parsed);
