208d32c2af412dc7319155fd3c2753a28cebbc60
Try Ajitiono <ballinst@gmail.com>
2025-02-26 12:01:47 +0000

    finish meter app

--- website/src/examples/meter/app.tsx
+++ website/src/examples/meter/app.tsx
@@ -1,46 +1,154 @@
-import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Slider } from '@/components/ui/slider';
+import { cn } from '@/lib/utils';
+import { useEffect, useState } from 'react';
+import { useForm, useWatch } from 'react-hook-form';
 
 export function MeterApp() {
+  const { control, formState, register, setValue } = useForm({
+    reValidateMode: 'onChange',
+    defaultValues: {
+      baseDamage: 100,
+      attacksPerSecond: 10,
+      critChance: 20,
+      critDamage: 150
+    }
+  });
+  const [result, setResult] = useState<number | undefined>();
+
+  const values = useWatch({ control });
+  useEffect(() => {
+    const asserted = values as Required<typeof values>;
+    const rawDPS = asserted.baseDamage * asserted.attacksPerSecond;
+    // 10000 is from crit chance and crit damage.
+    const xCritMultiplier = (asserted.critChance / 100) * (asserted.critDamage / 100 - 1);
+
+    setResult(rawDPS * (1 + xCritMultiplier));
+  }, [values]);
+
   return (
-    <main className="p-4 flex flex-col gap-y-4">
-      <h1 className="text-3xl font-bold mb-8">Meter App</h1>
+    <main className="p-4 flex flex-col gap-y-4 items-center justify-center w-full h-full">
+      <div className="max-w-96 w-full">
+        <h1 className="text-3xl font-bold mb-8 text-center">Meter App</h1>
 
-      <form
-        className="flex flex-col items-start gap-y-4"
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-      >
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="baseDamage">Base damage</Label>
-          <Input type="text" id="baseDamage" placeholder="1,500" />
-        </div>
+        <form className="flex flex-col items-center gap-y-4" onSubmit={(e) => e.preventDefault()}>
+          <div className={cn('grid w-full max-w-sm items-center gap-1.5', addErrorClassName(formState.errors.baseDamage?.message))}>
+            <Label htmlFor="baseDamage">Base damage</Label>
+            <Input
+              type="text"
+              id="baseDamage"
+              placeholder="1500"
+              {...register('baseDamage', {
+                required: true,
+                setValueAs: Number,
+                validate: (val) => {
+                  const parsed = Number(val);
+                  if (isNaN(parsed)) return 'Base damage should be a number';
+                  if (parsed < 0) return 'Base damage should be a positive number';
 
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="attacksPerSecond">Attacks per second</Label>
-          <Input type="text" id="attacksPerSecond" placeholder="10" />
-        </div>
+                  return undefined;
+                }
+              })}
+            />
 
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="critChance">Critical hit chance</Label>
+            <p className="text-xs">{formState.errors.baseDamage?.message}</p>
+          </div>
 
-          <Slider defaultValue={[50]} max={100} step={1} />
+          <div className={cn('grid w-full max-w-sm items-center gap-1.5', addErrorClassName(formState.errors.attacksPerSecond?.message))}>
+            <Label htmlFor="attacksPerSecond">Attacks per second</Label>
 
-          <Input type="text" id="critChance" placeholder="20" />
-        </div>
+            <Input
+              type="text"
+              id="attacksPerSecond"
+              placeholder="10"
+              {...register('attacksPerSecond', {
+                required: true,
+                setValueAs: Number,
+                validate: (val) => {
+                  const parsed = Number(val);
+                  if (isNaN(parsed)) return 'Attacks per second should be a number';
+                  if (parsed < 0) return 'Attacks per second should be a positive number';
 
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="critDamage">Critical hit % damage</Label>
-          <Input type="text" id="critDamage" placeholder="150" />
-        </div>
+                  return undefined;
+                }
+              })}
+            />
 
-        <Button variant="default" className="cursor-pointer mt-8">
-          Calculate
-        </Button>
-      </form>
+            <p className="text-xs">{formState.errors.attacksPerSecond?.message}</p>
+          </div>
+
+          <div className={cn('grid w-full max-w-sm items-center gap-1.5', addErrorClassName(formState.errors.critChance?.message))}>
+            <Label htmlFor="critChance">Critical hit chance</Label>
+
+            <div className="flex gap-x-2">
+              <Slider
+                value={[values.critChance!]}
+                max={100}
+                step={1}
+                onValueChange={(newValue) => {
+                  setValue('critChance', newValue[0]);
+                }}
+              />
+
+              <Input
+                type="text"
+                id="critChance"
+                placeholder="20"
+                className="max-w-16"
+                {...register('critChance', {
+                  required: true,
+                  setValueAs: Number,
+                  validate: (val) => {
+                    const parsed = Number(val);
+                    if (isNaN(parsed)) return 'Critical hit chance should be a number';
+                    if (parsed < 0) return 'Critical hit chance should be a positive number';
+
+                    return undefined;
+                  }
+                })}
+              />
+            </div>
+
+            <p className="text-xs">{formState.errors.critChance?.message}</p>
+          </div>
+
+          <div className={cn('grid w-full max-w-sm items-center gap-1.5', addErrorClassName(formState.errors.critDamage?.message))}>
+            <Label htmlFor="critDamage">Critical hit % damage</Label>
+
+            <Input
+              type="text"
+              id="critDamage"
+              placeholder="150"
+              {...register('critDamage', {
+                required: true,
+                setValueAs: Number,
+                validate: (val) => {
+                  const parsed = Number(val);
+                  if (isNaN(parsed)) return 'Critical hit damage should be a number';
+                  if (parsed <= 100) return 'Critical hit damage should be bigger than 100 (percent)';
+
+                  return undefined;
+                }
+              })}
+            />
+
+            <p className="text-xs">{formState.errors.critDamage?.message}</p>
+          </div>
+
+          {result !== undefined && (
+            <>
+              <hr className="border-gray-700 w-full my-4" />
+
+              <p>Your DPS is {Number.isSafeInteger(result) ? result : result.toFixed(2)} 🚀</p>
+            </>
+          )}
+        </form>
+      </div>
     </main>
   );
+}
+
+function addErrorClassName(errorMessage: string | undefined) {
+  return errorMessage ? '[&>label]:text-red-400 [&_input]:border-red-400 [&>p]:text-red-400' : undefined;
 }
