bd5e1251643c7021eab6baf2664b467943a8a7ce
Try Ajitiono <ballinst@gmail.com>
2025-02-26 13:43:01 +0000

    need to page.getByRole slider next, and maybe have a configuration for the fields

---
+++ src/index.meter.ts
@@ -0,0 +1,22 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { chromium } from 'playwright';
+import { parseInputTestFile } from './parser/input';
+import { runTests } from './runner';
+
+async function runPlaywrightExample() {
+  const testFileContent = await readFile(path.join(process.cwd(), 'website/src/examples/meter/test.yaml'), 'utf-8');
+
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto('http://localhost:4173/meter/');
+
+  const parsedTestFile = parseInputTestFile(testFileContent);
+  await runTests(page, parsedTestFile);
+
+  await browser.close();
+  console.info(`✅ All tests completed!`);
+}
+
+runPlaywrightExample();
