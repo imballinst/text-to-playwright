9492568dd61389dee32344d40062e520cf092188
Try Ajitiono <ballinst@gmail.com>
2025-06-22 16:21:33 +0700

    feat: allow waiting for URL using Playwright waitForURL (#25)

--- src/parser/input.test.ts
+++ src/parser/input.test.ts
@@ -39,6 +39,45 @@ tests:
     expect(parsed.tests[0].steps[1]).toBe(`Ensure "Output" element to contain text "hello"`);
   });
 
+  test('with waitForURL', () => {
+    let fileContent = `
+tests:
+  - name: Example test case
+    steps:
+      - Click "Users" menu, then click "Submit" button
+      - waitForURL: https://imballinst.dev
+    `.trim();
+
+    let parsed = parseInputTestFile(fileContent);
+
+    expect(parsed.tests.length).toBe(1);
+    expect(parsed.tests[0].name).toBe('Example test case');
+    expect(parsed.tests[0].steps.length).toBe(2);
+    expect(parsed.tests[0].steps[0]).toBe(`Click "Users" menu, then click "Submit" button`);
+    expect(parsed.tests[0].steps[1]).toStrictEqual({ kind: 'waitForURL', waitForURL: 'https://imballinst.dev' });
+
+    fileContent = `
+tests:
+  - name: Example test case
+    steps:
+      - Click "Users" menu, then click "Submit" button
+      - waitForURL: https://imballinst.dev
+        pageTitle: My personal website
+    `.trim();
+
+    parsed = parseInputTestFile(fileContent);
+
+    expect(parsed.tests.length).toBe(1);
+    expect(parsed.tests[0].name).toBe('Example test case');
+    expect(parsed.tests[0].steps.length).toBe(2);
+    expect(parsed.tests[0].steps[0]).toBe(`Click "Users" menu, then click "Submit" button`);
+    expect(parsed.tests[0].steps[1]).toStrictEqual({
+      kind: 'waitForURL',
+      waitForURL: 'https://imballinst.dev',
+      pageTitle: 'My personal website'
+    });
+  });
+
   test('with selectors on different levels', () => {
     let fileContent = `
 selector: data-qa-id
@@ -80,6 +119,7 @@ tests:
     expect(parsed.tests[0].steps.length).toBe(2);
     expect(parsed.tests[0].steps[0]).toBe(`Click "Users" menu, then click "Submit" button`);
     expect(parsed.tests[0].steps[1]).toStrictEqual({
+      kind: 'command',
       command: `Ensure "Output" element to contain text "hello"`,
       selector: 'data-qa-id',
       sliderSelector: 'shadcn'
